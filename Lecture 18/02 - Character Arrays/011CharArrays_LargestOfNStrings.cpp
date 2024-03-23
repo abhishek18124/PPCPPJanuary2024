3c7c5b3c26f10856efec92fe64b960e4d4b53b9f
abhishek18124 <abhishek18124@iiitd.ac.in>
2024-03-23 23:21:41 +0530

    Lecture 18

--- Lecture 18/02 - Character Arrays/011CharArrays_LargestOfNStrings.cpp
+++ Lecture 18/02 - Character Arrays/011CharArrays_LargestOfNStrings.cpp
@@ -1,4 +1,4 @@
-// strlen <= 100 
+// strlen <= 100
 
 #include<iostream>
 
@@ -9,6 +9,35 @@ int main() {
 	int n;
 	cout << "Enter the no. of strings : ";
 	cin >> n;
+
+	cin.ignore();
+	// cin.get();
+
+	char inp[101];
+	// cin >> inp;
+	cin.getline(inp, 101);
+
+	char lsf[101];
+	strcpy(lsf, inp);
+
+	for (int i = 1; i <= n - 1; i++) {
+
+		// cin >> inp;
+		cin.getline(inp, 101);
+
+		// cout << inp << endl;
+
+		if (strcmp(inp, lsf) > 0) {
+
+			// inp is greater than lsf
+
+			strcpy(lsf, inp);
+
+		}
+
+	}
+
+	cout << lsf << endl;
 
 	return 0;
 }
